0edb1517b395b86a1c9b6e1ab51442653a016355
Uttejh Reddy <uttejh@gmail.com>
2017-06-28 23:41:16 +0530

    removed values

--- visitorinform2.php
+++ visitorinform2.php
@@ -1,8 +1,8 @@
 <?php
 	header('Access-Control-Allow-Origin: *'); 
-	$servername = "31.170.166.61";
-	$username = "u903561932_root";
-	$password = "Theknight7";
+	$servername = "";
+	$username = "";
+	$password = "";
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password);
@@ -18,7 +18,7 @@
 	echo $data->type;
 
 	$flat=1;
-	$sql = "INSERT INTO u903561932_excit.temp_nv (type,flat)
+	$sql = "INSERT INTO temp_nv (type,flat)
 	VALUES ('".$data->type."','".$flat."')";
 
 	if ($conn->query($sql) === TRUE) {
